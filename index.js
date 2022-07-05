a3db6b2444aa5a7ab5c10b3abebab4b19fb171d5
Sayantan <this.sayantan@outlook.com>
2022-07-05 19:36:48 +0700

    WIP

--- index.js
+++ index.js
@@ -1,9 +1,3 @@
-console.log('js loaded');
-
-document.querySelector('#test').addEventListener('click', async event => {
-    console.log('working');
-});
-
 document.querySelector('#copy').addEventListener('click', async event => {
     if (!navigator.clipboard) {
         // Clipboard API not available
@@ -11,19 +5,39 @@ document.querySelector('#copy').addEventListener('click', async event => {
     }
     const text = document.querySelector('#textClipboard').value;
     const imageUrl = document.querySelector('#imageClipboard').value;
+    const textOnly = document.querySelector('#textOnly').checked;
     try {
         // copy text
-        await navigator.clipboard.writeText(text)
+        // await navigator.clipboard.writeText(text);
+        setClipboard(text);
         console.log('Text Copied to clipboard');
 
-        // copy image
-        await copyImage(imageUrl);
-        console.log('Image Copied to clipboard');
+        if (!textOnly) {
+            // copy image
+            await copyImage(imageUrl);
+            console.log('Image Copied to clipboard');
+        }
+
     } catch (err) {
         console.error('Failed to copy!', err)
     }
 });
 
+
+function setClipboard(text) {
+    const type = "text/plain";
+    const blob = new Blob([text], { type });
+    const data = [new ClipboardItem({ [type]: blob })];
+
+    navigator.clipboard.write(data).then(
+        function () {
+        /* success */
+        },
+        function () {
+        /* failure */
+        }
+    );
+}
 async function copyImage(url) {
     console.log("Wriing to clipbard");
 
@@ -50,18 +64,18 @@ document.querySelector('#paste').addEventListener('click', async event => {
         document.querySelector('#pasteHere').innerHTML = text;
 
         // image
-        const img = document.querySelector("#sample-img");
-        navigator.clipboard.read().then((data) => {
-            for (let i = 0; i < data.length; i++) {
-                if (!data[i].types.includes("image/png")) {
-                    // Clipboard does not contain image data
-                } else {
-                    data[i].getType("image/png").then((blob) => {
-                        img.src = URL.createObjectURL(blob);
-                    });
-                }
-            }
-        });
+        // const img = document.querySelector("#sample-img");
+        // navigator.clipboard.read().then((data) => {
+        //     for (let i = 0; i < data.length; i++) {
+        //         if (!data[i].types.includes("image/png")) {
+        //             // Clipboard does not contain image data
+        //         } else {
+        //             data[i].getType("image/png").then((blob) => {
+        //                 img.src = URL.createObjectURL(blob);
+        //             });
+        //         }
+        //     }
+        // });
     } catch (err) {
         console.error('Failed to copy!', err)
     }
